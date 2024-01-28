b9fa4de6111745fbf70fd8f24e83b7cfbb7afd6b
daameyaw <davidameyaw607@gmail.com>
2024-01-27 20:32:01 -0800

    change

--- js/views/questionsView.js
+++ js/views/questionsView.js
@@ -36,46 +36,48 @@ class questionsView extends View {
     this._parentElement.insertAdjacentHTML("afterbegin", markup);
   }
 
-  _generateMarkup() {
-    return `
-    ${this._data.map(
-      (question = function () {
-        this._generateMarkupForQuestion(question);
-      })
-    )};
-    `;
-  }
+  // _generateMarkup() {
+  //   return `
+  //   ${this._data.map(
+  //     (question = function () {
+  //       this._generateMarkupForQuestion(question);
+  //     })
+  //   )};
+  //   `;
+  // }
 
-  _generateMarkupForQuestion(question) {
-    return `<div class="quiz__number">Question 1/10</div>
-      <p class="question__text">${question._data.question}</p>
-      <button class="question__options html">
-        <p>${question._data.optionA}</p>
-      </button>
-      <button class="question__options"><p>${question._data.optionB}</p></button>
-      <button class="question__options">
-        <p>${question._data.optionC}</p>
-      </button>
-      <button class="question__options">
-        <p>${question._data.optionD}</p>
-      </button>
-      <button class="question__options">
-        <p>${question._data.optionE}</p>
-      </button>
-
-      <button class="question__options">
-        <p>${question._data.option}</p>
-      </button>
-      <div class="question__btns">
-        <div class="question__timer">4:00</div>
-        <div class="question__prevnext">
-          <button class="question__prevnext--prev hidden">previous</button>
-          <button class="question__prevnext--next hidden">next</button>
+  _generateMarkup(questions) {
+    const htmlArray = questions.map((question, i) => {
+      return `
+        <div class="quiz__number">Question ${i + 1}/10</div>
+        <p class="question__text">${question.question}</p>
+        <button class="question__options html">
+          <p>${question.optionA}</p>
+        </button>
+        <button class="question__options"><p>${question.optionB}</p></button>
+        <button class="question__options">
+          <p>${question.optionC}</p>
+        </button>
+        <button class="question__options">
+          <p>${question.optionD}</p>
+        </button>
+        <button class="question__options">
+          <p>${question.optionE}</p>
+        </button>
+  
+        <div class="question__btns">
+          <div class="question__timer">4:00</div>
+          <div class="question__prevnext">
+            <button class="question__prevnext--prev ">previous</button>
+            <button class="question__prevnext--next ">next</button>
+          </div>
         </div>
-      </div>
-      <div class="quiz__finish">finish</div>
-      <button class="quiz__submit">submit</button>
-  `;
+        <div class="quiz__finish">finish</div>
+        <button class="quiz__submit">submit</button>
+      `;
+    });
+
+    return htmlArray.join("");
   }
 }
 
