89abb0fde0d7f37825c73ce7ad082fcc282e9a14
daameyaw <davidameyaw607@gmail.com>
2024-02-07 22:22:12 -0800

    cd

--- js/views/view.js
+++ js/views/view.js
@@ -18,40 +18,13 @@ export default class View {
     this._parentElement.insertAdjacentHTML("afterbegin", markup);
   }
 
-  // _clear() {
-  //   this._parentElement.innerHTML = "";
-  // }
-
-  //       if (
-  //         !newEl.isEqualNode(curEl) &&
-  //         newEl.firstChild?.nodeValue.trim() !== ''
-  //       ) {
-  //         curEl.textContent = newEl.textContent;
-  //       }
-  //       if (!newEl.isEqualNode(curEl)) {
-  //         Array.from(newEl.attributes).forEach(attribute =>
-  //           curEl.setAttribute(attribute.name, attribute.value)
-  //         );
-  //       }
-  //     });
-  //   }
+  _clear() {
+    this._parentElement.innerHTML = "";
+  }
 
   //   addHandlerRender(handler) {
   //     ['hashchange', 'load'].forEach(function (event) {
   //       window.addEventListener(event, handler);
   //     });
-  //   }
-
-  //   renderMessage(message = this._message) {
-  //     const markup = `<div class="message">
-  //         <div>
-  //                <svg>
-  //                   <use href="${icons}#icon-smile"></use>
-  //              </svg>
-  //         </div>
-  //            <p>${message}</p>
-  //          </div>`;
-  //     this._clear();
-  //     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   //   }
 }
