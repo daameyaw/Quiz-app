de38a701bf464cd7d72cdde938485ca648067782
daameyaw <davidameyaw607@gmail.com>
2024-02-18 14:55:59 -0800

    cds

---
+++ js/views/optionsView.js
@@ -0,0 +1,16 @@
+import View from "./view.js";
+
+class optionsView extends View {
+  _parentElement = document.querySelector(".question");
+
+  handleMarking(handler) {
+    this._parentElement.addEventListener("click", function (e) {
+      const option = e.target.closest(".question__options");
+      if (!option) return;
+      const selectedOption = option.dataset.option;
+      handler(selectedOption);
+    });
+  }
+}
+
+export default new optionsView();
