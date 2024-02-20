23e5a064e7f4dccd41978595a76fdfef51c8a61b
daameyaw <davidameyaw607@gmail.com>
2024-02-19 18:44:58 -0800

    changes

--- js/views/view.js
+++ js/views/view.js
@@ -18,6 +18,13 @@ export default class View {
     this._parentElement.insertAdjacentHTML("afterbegin", markup);
   }
 
+  renderSubmit() {
+    const markup = this._generateMarkupP();
+
+    this._clear();
+    this._parentElement.insertAdjacentHTML("afterbegin", markup);
+  }
+
   _clear() {
     this._parentElement.innerHTML = "";
   }
