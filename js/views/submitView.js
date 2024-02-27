19c14e92f080f2a8ad8f28a574d3d8da7f45cac1
daameyaw <davidameyaw607@gmail.com>
2024-02-26 19:55:12 -0800

    changes

--- js/views/submitView.js
+++ js/views/submitView.js
@@ -1,7 +1,16 @@
 import View from "./view.js";
 
+const decisionPopup = document.querySelector(".submit");
+
 class submitView extends View {
   _parentElement = document.querySelector(".decision");
+
+  handleSubmitBtn(handler) {
+    this._parentElement.addEventListener("click", function (e) {
+      decisionPopup.classList.remove("hidden");
+      handler();
+    });
+  }
 
   _generateMarkupP() {
     return `<div class="quiz__finish">finish</div>
