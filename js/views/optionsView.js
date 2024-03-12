749c09296689e43014c2cd05a3aaa8ac9f793d88
daameyaw <davidameyaw607@gmail.com>
2024-03-11 22:15:30 -0800

    changes

--- js/views/optionsView.js
+++ js/views/optionsView.js
@@ -3,14 +3,14 @@ import View from "./view.js";
 class optionsView extends View {
   _parentElement = document.querySelector(".question");
 
-  handleMarking(handler) {
-    this._parentElement.addEventListener("click", function (e) {
-      const option = e.target.closest(".question__options");
-      if (!option) return;
-      const selectedOption = option.dataset.option;
-      handler(selectedOption);
-    });
-  }
+  // handleMarking(handler) {
+  //   this._parentElement.addEventListener("click", function (e) {
+  //     const option = e.target.closest(".question__options");
+  //     if (!option) return;
+  //     const selectedOption = option.dataset.option;
+  //     handler(selectedOption);
+  //   });
+  // }
 }
 
 export default new optionsView();
