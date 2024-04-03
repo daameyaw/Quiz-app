b85f1daf59defe12ebeb8010244ff34227d5e130
daameyaw <davidameyaw607@gmail.com>
2024-04-03 13:03:04 -0800

    cds

--- js/views/timerView.js
+++ js/views/timerView.js
@@ -16,15 +16,14 @@ class timerView extends View {
       const minutes = String(Math.floor(timeLeft / 60)).padStart(2, 0);
       const seconds = String(timeLeft % 60).padStart(2, 0);
 
-      // console.log(minutes, seconds);
       timerEl.textContent = `${minutes}:${seconds}`;
-      console.log(timerEl.textContent);
 
       timeLeft--;
 
       if (timeLeft < 0) {
         clearInterval(timer);
         timerEl.textContent = "Time up!";
+        model.state.done = true;
         decisionPopup.classList.remove("hidden");
         goBackBtn.disabled = true;
       }
@@ -37,22 +36,17 @@ class timerView extends View {
     };
 
     const totalTime = model.state.questions.length * 30; // 5 minutes in seconds
-    // const totalTime = 10;
+    // const totalTime = 10; // 5 minutes in seconds
 
     let timeLeft = totalTime;
 
     const timerEl = document.querySelector(".question__timer");
-    console.log(timerEl);
 
     countdown();
     const timer = setInterval(countdown, 1000);
 
-    console.log(timer);
     return timer;
   }
-  //   stopTimer() {
-  //     clearInterval(countdown);
-  //   }
 
   _generateMarkupP() {
     return `<div class="question__timer">5:00</div>`;
